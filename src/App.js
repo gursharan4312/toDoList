4248b5908e2e75f5113996d0f4d9e97dff3ee540
Gursharan Singh <gursharansingh4312@gmail.com>
2019-01-14 11:30:10 -0800

    same id issue solved

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ componentDidMount(){
 // toggle complete state
 markComplete = (id)=>{
   this.setState({ todos: this.state.todos.map(todo=>{
-    if(todo.id===id){
-      todo.completed = !todo.completed;
-    }
+        if(todo.id===id){
+          todo.completed = !todo.completed;
+        }
     return todo;
   }) })
 }
@@ -39,7 +39,10 @@ addTodo = (title)=>{
     title,
     complete:false
   })
-  .then(res=>  this.setState({todos:[...this.state.todos,res.data]}));
+  .then((res)=>  {
+    res.data.id = this.state.todos.length + 1;
+    this.setState({todos:[...this.state.todos,res.data]});
+  });
 
 }
 
@@ -51,18 +54,18 @@ addTodo = (title)=>{
             <Header />
 
             <Route exact path="/" render={props=>(
-              <React.Fragment>
-              <AddTodo addTodo={this.addTodo}/>
-              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
-              </React.Fragment>
-            )} />
+                    <React.Fragment>
+                    <AddTodo addTodo={this.addTodo}/>
+                    <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                    </React.Fragment>
+                  )}
+            />
 
             <Route path="/about" component={About} />
 
           </div>
         </div>
       </Router>
-
     );
   }
 }
